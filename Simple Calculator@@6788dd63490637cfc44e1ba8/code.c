2f6d257faeb5e4d60a10325c54238b6a4292a554
Prabhu99Bansal <prabhu0071.becse24@chitkara.edu.in>
2025-01-23 09:36:22 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,23 +2,26 @@
 int main(){
     int a,b;
     char c;
-
     scanf("%d",&a);
     scanf("%d",&b);
     scanf("% c",&c);
-    if (c=='+'){
+    switch (c){
+        case '+':
         printf("%d",a+b);
-    }
-    else if(c=='-'){
+        break;
+        case '-':
         printf("%d",a-b);
-    }
-    else if(c=='*'){
+        break;
+        case '*'':
         printf("%d",a*b);
-
-    }
-    else{
+        break;
+        case '/':
         printf("%d",a/b);
-    }
+        break;
+        default:
+        printf("Invalid");
+        break;
+        }
     return 0;
 
 
