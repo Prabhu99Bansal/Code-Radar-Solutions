ae6eecedfd0b5eca984f1dc0be288c11ca095adb
Prabhu99Bansal <prabhu0071.becse24@chitkara.edu.in>
2025-02-07 10:13:21 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -17,7 +17,7 @@ int main(){
         break;
         case '/':
         if (b!=0){
-            printf("%.2f",a/b);
+            printf("%.2f",(float)a/b);
         }
         else{
             printf("error");
